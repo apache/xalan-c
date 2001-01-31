7e746589fdb3eb00163ca755987eeb872e62ec66
Stephen Auriemma <auriemma@apache.org>
2001-01-31 14:30:54 +0000

    New file used to implement a simple C++ interface for some common usage patterns.

---
+++ src/XalanTransformer/XalanTransformer.cpp
@@ -0,0 +1,253 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, International
+ * Business Machines, Inc., http://www.ibm.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+#include <util/PlatformUtils.hpp>
+
+
+
+#include <XalanTransformer/XalanTransformer.hpp>
+
+
+
+#include <PlatformSupport/XalanOutputStreamPrintWriter.hpp>
+
+
+
+XSLTInit*	XalanTransformer::m_xsltInit = 0;
+
+
+
+XalanTransformer::XalanTransformer():
+	m_domSupport(),
+	m_parserLiaison(m_domSupport),
+	m_xsltprocessorEnvSupport(),
+	m_xobjectFactory(),
+	m_xpathFactory(),
+	m_processor(m_parserLiaison, 
+				m_xsltprocessorEnvSupport,
+				m_domSupport,
+				m_xobjectFactory,
+				m_xpathFactory),
+	m_stylesheetConstructionContext(
+				m_processor,
+				m_xsltprocessorEnvSupport,
+				m_xpathFactory),
+	m_stylesheetExecutionContext(
+				m_processor,
+				m_xsltprocessorEnvSupport,
+				m_domSupport,
+				m_xobjectFactory)
+{
+	m_domSupport.setParserLiaison(&m_parserLiaison);
+	m_xsltprocessorEnvSupport.setProcessor(&m_processor);
+}
+
+
+
+XalanTransformer::~XalanTransformer()
+{
+}
+
+
+
+void
+XalanTransformer::initialize()
+{
+	// Call the static initializer for Xerces.
+	XMLPlatformUtils::Initialize();
+
+	// Initialize Xalan. 
+	XalanTransformer::m_xsltInit = new XSLTInit;
+}
+
+
+
+void
+XalanTransformer::terminate()
+{
+	// Terminate Xalan and release memory.
+	delete XalanTransformer::m_xsltInit;
+
+	// Call the static terminator for Xerces.
+	XMLPlatformUtils::Terminate();
+}
+
+
+
+int
+XalanTransformer::transform(
+	const XSLTInputSource&		theInputSource, 
+	const XSLTInputSource&		theStylesheetSource,
+	XSLTResultTarget&			theResultTarget)
+{
+	try
+	{
+		// Do the transformation...
+		m_processor.process(
+					theInputSource,
+					theStylesheetSource,
+					theResultTarget,
+					m_stylesheetConstructionContext,
+					m_stylesheetExecutionContext);
+	
+		// Reset objects.
+		m_stylesheetExecutionContext.reset();		
+		m_parserLiaison.reset();			
+	}
+	catch(...)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+
+
+int
+XalanTransformer::transform(
+		const char*		theXMLFileName, 
+		const char*		theXSLFileName,
+		const char*		theOutFileName)
+{
+	// Set input sources
+	XSLTInputSource	theInputSource(c_wstr(XalanDOMString(theXMLFileName)));
+	XSLTInputSource	theStylesheetSource(c_wstr(XalanDOMString(theXSLFileName)));
+
+	// Set output target
+	const XalanDOMString	theDomStringOutFileName(theOutFileName);
+	
+	XSLTResultTarget		theResultTarget(theDomStringOutFileName);
+
+	// Do the transformation...
+	return transform(
+					theInputSource, 
+					theStylesheetSource,
+					theResultTarget);
+}
+
+
+
+int
+XalanTransformer::transform(
+		const char*		theXMLFileName, 
+		const char*		theXSLFileName,
+		ostream*		theOutStream)
+{
+	// Set input sources
+	XSLTInputSource		theInputSource(c_wstr(XalanDOMString(theXMLFileName)));
+	XSLTInputSource		theStylesheetSource(c_wstr(XalanDOMString(theXSLFileName)));
+
+	// Set output target
+	XSLTResultTarget	theResultTarget(theOutStream);
+
+	// Do the transformation...
+	return transform(
+					theInputSource, 
+					theStylesheetSource,
+					theResultTarget);
+}
+
+
+
+int
+XalanTransformer::transform(
+		istream*		theXMLInStream, 
+		istream*		theXSLInStream,
+		ostream*		theOutStream)
+{
+	// Set input sources
+	XSLTInputSource		theInputSource(theXMLInStream);
+	XSLTInputSource		theStylesheetSource(theXSLInStream);
+	
+	// Set output target
+	XSLTResultTarget	theResultTarget(theOutStream);
+
+	// Do the transformation...
+	return transform(
+					theInputSource, 
+					theStylesheetSource,
+					theResultTarget);
+}
+
+
+
+int
+XalanTransformer::transform(
+		const char*				theXMLFileName, 
+		const char*				theXSLFileName,
+		const void*				theOutputHandle, 
+		XalanOutputHandlerType	theOutputHandler)
+{
+	// Set input sources
+	XSLTInputSource	theInputSource(c_wstr(XalanDOMString(theXMLFileName)));
+	XSLTInputSource	theStylesheetSource(c_wstr(XalanDOMString(theXSLFileName)));
+
+	// Set to output target to the callback 
+	XalanTransformerOutputStream	theOutputStream(theOutputHandle, theOutputHandler);
+	XalanOutputStreamPrintWriter	thePrintWriter(theOutputStream);
+	XSLTResultTarget				theResultTarget(&thePrintWriter);
+
+	// Do the transformation...
+	return transform(
+					theInputSource, 
+					theStylesheetSource,
+					theResultTarget);
+}
+
+
+
+
