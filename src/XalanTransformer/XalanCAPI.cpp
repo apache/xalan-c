ab43999ca844d2416e67724cb74d68c910075a16
David N Bertoni <dbertoni@apache.org>
2001-06-07 14:44:35 +0000

    Fixed header file name.

--- src/XalanTransformer/XalanCAPI.cpp
+++ src/XalanTransformer/XalanCAPI.cpp
@@ -54,7 +54,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
-#include "XalanTransformerDefinitions"
+#include "XalanTransformerDefinitions.hpp"
 
 
 
