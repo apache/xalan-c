90c832c17a0fb487e9aeb0d787ef4d0adb7d4fff
David N Bertoni <dbertoni@apache.org>
2002-05-08 01:39:57 +0000

    Fixed cast problem with older compilers.

--- src/XSLT/StylesheetExecutionContextDefault.cpp
+++ src/XSLT/StylesheetExecutionContextDefault.cpp
@@ -1162,7 +1162,7 @@ StylesheetExecutionContextDefault::returnXResultTreeFrag(XResultTreeFrag*	theXRe
 		{
 #if defined(XALAN_NO_COVARIANT_RETURN_TYPE)
 #if defined(XALAN_OLD_STYLE_CASTS)
-			m_documentAllocator.destroy((const XalanSourceTreeDocument*)theDocumentFragment->getOwnerDocument());
+			m_documentAllocator.destroy((XalanSourceTreeDocument*)theDocumentFragment->getOwnerDocument());
 #else
 			m_documentAllocator.destroy(static_cast<XalanSourceTreeDocument*>(theDocumentFragment->getOwnerDocument()));
 #endif
