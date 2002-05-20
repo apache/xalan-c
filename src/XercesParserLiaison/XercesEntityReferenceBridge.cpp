4a4e3b67c7565a416416a28a3867e7780b0e6017
David N Bertoni <dbertoni@apache.org>
2002-05-20 22:54:21 +0000

    Branch for old Xerces includes.

--- src/XercesParserLiaison/XercesEntityReferenceBridge.cpp
+++ src/XercesParserLiaison/XercesEntityReferenceBridge.cpp
@@ -258,7 +258,7 @@ XercesEntityReferenceBridge::isSupported(
 			const XalanDOMString&	feature,
 			const XalanDOMString&	version) const
 {
-	return m_xercesNode.isSupported(
+	return m_xercesNode.supports(
 				XercesBridgeHelper::XalanDOMStringToXercesDOMString(feature),
 				XercesBridgeHelper::XalanDOMStringToXercesDOMString(version));
 }
