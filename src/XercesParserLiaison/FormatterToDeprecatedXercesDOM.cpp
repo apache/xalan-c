8363b5bf6067b439c05ff98c5b6ba0d22a747f1e
Berin Lautenbach <blautenb@apache.org>
2003-02-12 08:33:46 +0000

    FormatterListener for deprecated Xerces DOM

---
+++ src/XercesParserLiaison/FormatterToDeprecatedXercesDOM.cpp
@@ -0,0 +1,473 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, International
+ * Business Machines, Inc., http://www.ibm.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+// Class header file.
+#include "FormatterToDeprecatedXercesDOM.hpp"
+
+
+
+#include <cassert>
+
+
+
+#include <xercesc/sax/AttributeList.hpp>
+
+
+
+#if XERCES_VERSION_MAJOR >= 2
+#include <xercesc/dom/deprecated/DOM_CDATASection.hpp>
+#include <xercesc/dom/deprecated/DOM_Comment.hpp>
+#include <xercesc/dom/deprecated/DOM_EntityReference.hpp>
+#include <xercesc/dom/deprecated/DOM_ProcessingInstruction.hpp>
+#include <xercesc/dom/deprecated/DOM_Text.hpp>
+#else
+#include <xercesc/dom/DOM_CDATASection.hpp>
+#include <xercesc/dom/DOM_Comment.hpp>
+#include <xercesc/dom/DOM_EntityReference.hpp>
+#include <xercesc/dom/DOM_ProcessingInstruction.hpp>
+#include <xercesc/dom/DOM_Text.hpp>
+#endif
+
+
+#include <XalanDOM/XalanDOMString.hpp>
+
+
+
+#include <PlatformSupport/DOMStringHelper.hpp>
+#include <PlatformSupport/PrefixResolver.hpp>
+
+
+
+XALAN_CPP_NAMESPACE_BEGIN
+
+
+
+const XalanDOMString	FormatterToDeprecatedXercesDOM::s_emptyString;
+
+
+
+FormatterToDeprecatedXercesDOM::FormatterToDeprecatedXercesDOM(
+			DOM_Document_Type&			doc,
+			DOM_DocumentFragmentType&	docFrag,
+			DOM_ElementType&			currentElement) :
+	FormatterListener(OUTPUT_METHOD_DOM),
+	m_doc(doc),
+	m_docFrag(docFrag),
+	m_currentElem(currentElement),
+	m_elemStack(),
+	m_buffer(),
+	m_textBuffer()
+{
+	assert(m_doc != 0 && m_docFrag != 0);
+}
+
+
+
+FormatterToDeprecatedXercesDOM::FormatterToDeprecatedXercesDOM(
+			DOM_Document_Type&	    doc,
+			DOM_ElementType&		elem) :
+	FormatterListener(OUTPUT_METHOD_DOM),
+	m_doc(doc),
+	m_docFrag(),
+	m_currentElem(elem),
+	m_elemStack(),
+	m_buffer(),
+	m_textBuffer()
+{
+	assert(m_doc != 0);
+}
+
+FormatterToDeprecatedXercesDOM::FormatterToDeprecatedXercesDOM(
+			DOM_Document_Type&	    doc) :
+	FormatterListener(OUTPUT_METHOD_DOM),
+	m_doc(doc),
+	m_docFrag(),
+	m_currentElem(),
+	m_elemStack(),
+	m_buffer(),
+	m_textBuffer()
+{
+	assert(m_doc != 0);
+}
+
+
+
+FormatterToDeprecatedXercesDOM::~FormatterToDeprecatedXercesDOM()
+{
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::setDocumentLocator(const LocatorType* const	/* locator */)
+{
+	// No action for the moment.
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::startDocument()
+{
+	// No action for the moment.
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::endDocument()
+{
+	// No action for the moment.
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::startElement(
+			const	XMLCh* const	name,
+			AttributeListType&		attrs)
+{
+	processAccumulatedText();
+
+	DOM_ElementType elem = createElement(name, attrs);
+
+	append(elem);
+
+	m_elemStack.push_back(m_currentElem);
+
+	m_currentElem = elem;
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::endElement(const	XMLCh* const	/* name */)
+{
+	processAccumulatedText();
+
+	if(m_elemStack.empty() == false)
+	{
+		m_currentElem = m_elemStack.back();
+
+		m_elemStack.pop_back();
+	}
+	else
+	{
+		m_currentElem = 0;
+	}
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::characters(
+			const XMLCh* const	chars,
+			const unsigned int	length)
+{
+	m_textBuffer.append(chars, length);
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::charactersRaw(
+		const XMLCh* const	chars,
+		const unsigned int	length)
+{
+	processAccumulatedText();
+
+	cdata(chars, length);
+}		
+
+
+
+void
+FormatterToDeprecatedXercesDOM::entityReference(const XMLCh* const	name)
+{
+	processAccumulatedText();
+
+	DOM_EntityReferenceType  theXercesNode =
+		m_doc.createEntityReference(name);
+
+	assert(theXercesNode.isNull() == false);
+
+	append(theXercesNode);
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::ignorableWhitespace(
+			const XMLCh* const	chars,
+			const unsigned int	length)
+{
+	processAccumulatedText();
+
+	assign(m_buffer, chars, length);
+
+	DOM_TextType theXercesNode = 
+		m_doc.createTextNode(m_buffer.c_str());
+
+	assert(theXercesNode.isNull() == false);
+
+	append(theXercesNode);
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::processingInstruction(
+			const XMLCh* const	target,
+			const XMLCh* const	data)
+{
+	processAccumulatedText();
+
+	DOM_ProcessingInstructionType theXercesNode = 
+		m_doc.createProcessingInstruction(target, data);
+
+	assert(theXercesNode.isNull() == false);
+
+	append(theXercesNode);
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::resetDocument()
+{
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::comment(const XMLCh* const	data)
+{
+	processAccumulatedText();
+
+	DOM_CommentType theXercesNode =
+		m_doc.createComment(data);
+
+	assert(theXercesNode.isNull() == false);
+
+	append(theXercesNode);
+
+
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::cdata(
+			const XMLCh* const	ch,
+			const unsigned int 	length)
+{
+	processAccumulatedText();
+
+	assign(m_buffer, ch, length);
+
+	DOM_CDATASectionType theXercesNode = 
+		m_doc.createCDATASection(m_buffer.c_str());
+
+	assert(theXercesNode.isNull() == false);
+
+	append(theXercesNode);
+
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::append(DOM_NodeType	&newNode)
+{
+	assert(newNode != 0);
+
+	if(m_currentElem.isNull() == false)
+	{
+		m_currentElem.appendChild(newNode);
+	}
+	else if(m_docFrag.isNull() == false)
+	{
+		m_docFrag.appendChild(newNode);
+	}
+	else
+	{
+		m_doc.appendChild(newNode);
+	}
+}
+
+
+
+DOM_ElementType
+FormatterToDeprecatedXercesDOM::createElement(
+			const XalanDOMChar*		theElementName,
+			AttributeListType&		attrs)
+{
+	DOM_ElementType	theElement;
+
+	if (m_prefixResolver == 0)
+	{
+		theElement = m_doc.createElement(theElementName);
+
+		addAttributes(theElement, attrs);
+	}
+	else
+	{
+		// Check for the namespace...
+		const XalanDOMString* const		theNamespace =
+					getNamespaceForPrefix(theElementName, *m_prefixResolver, m_buffer);
+
+		if (theNamespace == 0 || length(*theNamespace) == 0)
+		{
+			theElement = m_doc.createElement(theElementName);
+		}
+		else
+		{
+			theElement = m_doc.createElementNS(theNamespace->c_str(), theElementName);
+		}
+
+		addAttributes(theElement, attrs);
+	}
+
+	return theElement;
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::addAttributes(
+			DOM_ElementType&	theElement,
+			AttributeListType&	attrs)
+{
+	const unsigned int	nAtts = attrs.getLength();
+
+	if (m_prefixResolver == 0)
+	{
+		for(unsigned int i = 0; i < nAtts; i++)
+		{
+			theElement.setAttribute(attrs.getName(i), attrs.getValue(i));
+		}
+	}
+	else
+	{
+		for(unsigned int i = 0; i < nAtts; i++)
+		{
+			const XalanDOMChar* const	theName = attrs.getName(i);
+			assert(theName != 0);
+
+			// Check for the namespace...
+			const XalanDOMString* const		theNamespace =
+					getNamespaceForPrefix(theName, *m_prefixResolver, m_buffer);
+
+			if (theNamespace == 0 || length(*theNamespace) == 0)
+			{
+				theElement.setAttribute(theName, attrs.getValue(i));
+			}
+			else
+			{
+				theElement.setAttributeNS(theNamespace->c_str(), theName, attrs.getValue(i));
+			}
+		}
+	}
+}
+
+
+
+const XalanDOMString*
+FormatterToDeprecatedXercesDOM::getNamespaceForPrefix(
+			const XalanDOMChar*		theName,
+			const PrefixResolver&	thePrefixResolver,
+			XalanDOMString&			thePrefix)
+{
+	const XalanDOMString::size_type		theLength = length(theName);
+	const XalanDOMString::size_type		theColonIndex = indexOf(theName, XalanUnicode::charColon);
+
+	if (theColonIndex == theLength)
+	{
+		clear(thePrefix);
+
+		return thePrefixResolver.getNamespaceForPrefix(s_emptyString);
+	}
+	else
+	{
+		// Get the prefix from theName...
+		assign(thePrefix, theName, theColonIndex);
+		assert(length(thePrefix) != 0);
+
+		return thePrefixResolver.getNamespaceForPrefix(thePrefix);
+	}
+}
+
+
+
+void
+FormatterToDeprecatedXercesDOM::processAccumulatedText()
+{
+	if (isEmpty(m_textBuffer) == false)
+	{
+		DOM_TextType theXercesNode =
+			m_doc.createTextNode(m_textBuffer.c_str());
+		
+		assert(theXercesNode.isNull() == false);
+
+		append(theXercesNode);
+
+		clear(m_textBuffer);
+	}
+}
+
+
+
+XALAN_CPP_NAMESPACE_END
---
+++ src/XercesParserLiaison/FormatterToDeprecatedXercesDOM.hpp
@@ -0,0 +1,304 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999-2003 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, International
+ * Business Machines, Inc., http://www.ibm.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+#if !defined(FORMATTERTODEPRECATEDXERCESDOM_HEADER_GUARD_1357924680)
+#define FORMATTERTODEPRECATEDXERCESDOM_HEADER_GUARD_1357924680
+
+
+
+// Base include file.  Must be first.
+#include <XercesParserLiaison/XercesParserLiaisonDefinitions.hpp>
+
+
+
+#include <vector>
+
+
+
+// Base class header file.
+#include <PlatformSupport/FormatterListener.hpp>
+
+
+
+#include <XalanDOM/XalanDOMString.hpp>
+
+
+
+#include <XercesParserLiaison/XercesBridgeTypes.hpp>
+
+// Required Xerces headers
+
+#if XERCES_VERSION_MAJOR >= 2
+#include <xercesc/dom/deprecated/DOM_Document.hpp>
+#include <xercesc/dom/deprecated/DOM_DocumentFragment.hpp>
+#include <xercesc/dom/deprecated/DOM_Element.hpp>
+#else
+#include <xercesc/dom/DOM_Document.hpp>
+#include <xercesc/dom/DOM_DocumentFragment.hpp>
+#include <xercesc/dom/DOM_Element.hpp>
+#endif
+
+
+XALAN_CPP_NAMESPACE_BEGIN
+
+
+
+/**
+ * This class takes SAX events (in addition to some extra events that SAX
+ * doesn't handle yet) and adds the result to a document or document fragment.
+ *
+ * It is provided as assist developers transition away from the deprecated DOM.
+ */
+class XALAN_XERCESPARSERLIAISON_EXPORT FormatterToDeprecatedXercesDOM : public FormatterListener
+{
+public:
+
+	/**
+	 * Construct a FormatterToDeprecatedXercesDOM instance.  it will add the DOM nodes 
+	 * to the document fragment.
+	 *
+	 * @deprecated This API is deprecated and is only provided as a transition tool
+	 * @param doc            document for nodes
+	 * @param docFrag        document fragment for nodes
+	 * @param currentElement current element for nodes
+	 */
+	FormatterToDeprecatedXercesDOM(
+			DOM_Document_Type			&doc,
+			DOM_DocumentFragmentType	&docFrag,
+			DOM_ElementType			    &currentElement);
+
+	/**
+	 * Construct a FormatterToDeprecatedXerces DOM instance.  it will add the DOM nodes 
+	 * to the document.
+	 *
+	 * @deprecated This API is deprecated and is only provided as a transition tool
+	 * @param doc  document for nodes
+	 * @param elem current element for nodes
+	 */
+	FormatterToDeprecatedXercesDOM(
+			DOM_Document_Type			&doc,
+			DOM_ElementType 			&currentElement);
+
+	/**
+	 * Construct a FormatterToDeprecatedXercesDOM instance.  it will add the DOM nodes 
+	 * to the document.
+	 *
+	 * @deprecated This API is deprecated and is only provided as a transition tool
+	 * @param doc  document for nodes
+	 */
+	FormatterToDeprecatedXercesDOM(
+ 		    DOM_Document_Type			&doc);
+
+	virtual
+	~FormatterToDeprecatedXercesDOM();
+
+
+	// These methods are inherited from DocumentHandler ...
+
+	virtual void
+	charactersRaw(
+			const XMLCh* const	chars,
+			const unsigned int	length);
+
+	virtual void
+	comment(const XMLCh* const	data);
+
+	virtual void
+	cdata(
+			const XMLCh* const	ch,
+			const unsigned int 	length);
+
+	virtual void
+	entityReference(const XMLCh* const	name);
+
+	virtual void
+	setDocumentLocator(const LocatorType* const		locator);
+
+	virtual void
+	startDocument();
+
+	virtual void
+	endDocument();
+
+	virtual void
+	startElement(
+				const	XMLCh* const	name,
+				AttributeListType&		attrs);
+
+	virtual void
+	endElement(const XMLCh* const	name);
+
+	virtual void
+	characters(
+				const XMLCh* const	chars,
+				const unsigned int	length);
+
+	virtual void
+	ignorableWhitespace(
+				const XMLCh* const	chars,
+				const unsigned int	length);
+
+	virtual void
+	processingInstruction(
+			const XMLCh* const	target,
+			const XMLCh* const	data);
+
+	virtual void
+	resetDocument();
+
+	DOM_Document_Type
+	getDocument() const
+	{
+		return m_doc;
+	}
+
+	void
+	setDocument(DOM_Document_Type	&theDocument)
+	{
+		m_doc = theDocument;
+	}
+
+	DOM_DocumentFragmentType
+	getDocumentFragment() const
+	{
+		return m_docFrag;
+	}
+
+	void
+	setDocumentFragment(DOM_DocumentFragmentType	&theDocumentFragment)
+	{
+		m_docFrag = theDocumentFragment;
+	}
+
+	DOM_ElementType
+	getCurrentElement() const
+	{
+		return m_currentElem;
+	}
+
+	void
+	setCurrentElement(DOM_ElementType		&theElement)
+	{
+		m_currentElem = theElement;
+	}
+
+private:
+
+	/**
+	 * Process any accumulated text and create a node for it.
+	 */
+	void
+	processAccumulatedText();
+
+	/**
+	 * Append a node to the current container.
+	 */
+	void
+	append(DOM_NodeType 	&newNode);
+
+	/**
+	 * Create the appropriate element, complete with attributes set.
+	 *
+	 * @param theElementName The name for the new element
+	 * @param attrs The SAX AttributeList for the new attributes.
+	 * @return A pointer to the new instance.
+	 */
+	DOM_ElementType
+	createElement(
+			const XalanDOMChar*		theElementName,
+			AttributeListType&		attrs);
+
+	void
+	addAttributes(
+			DOM_ElementType 		&theElement,
+			AttributeListType&	attrs);
+
+	const XalanDOMString*
+	getNamespaceForPrefix(
+			const XalanDOMChar*		theName,
+			const PrefixResolver&	thePrefixResolver,
+			XalanDOMString&			thePrefix);
+
+
+	// Data members...
+	DOM_Document_Type				m_doc;
+
+	DOM_DocumentFragmentType		m_docFrag;
+
+	DOM_ElementType					m_currentElem;
+
+#if defined(XALAN_NO_STD_NAMESPACE)
+	typedef vector<DOM_ElementType>			ElementStackType;
+#else
+	typedef std::vector<DOM_ElementType>	ElementStackType;
+#endif
+
+	ElementStackType				m_elemStack;
+
+	XalanDOMString					m_buffer;
+
+	XalanDOMString					m_textBuffer;
+
+	static const XalanDOMString		s_emptyString;
+};
+
+
+
+XALAN_CPP_NAMESPACE_END
+
+
+
+#endif	// FORMATTERTODEPRECATEDXERCESDOM_HEADER_GUARD_1357924680
