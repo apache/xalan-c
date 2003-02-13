fdfdbcfe566f229b203ac6cb5284c97a8251a546
David N Bertoni <dbertoni@apache.org>
2003-02-13 02:37:44 +0000

    Moved using declaration after function declarations.

--- src/XalanExtensions/FunctionIntersection.hpp
+++ src/XalanExtensions/FunctionIntersection.hpp
@@ -89,16 +89,16 @@ public:
 
 	// These methods are inherited from Function ...
 
-#if !defined(XALAN_NO_USING_DECLARATION)
-	using ParentType::execute;
-#endif
-
 	virtual XObjectPtr
 	execute(
 			XPathExecutionContext&			executionContext,
 			XalanNode*						context,
 			const XObjectArgVectorType&		args,
 			const LocatorType*				locator) const;
+
+#if !defined(XALAN_NO_USING_DECLARATION)
+	using ParentType::execute;
+#endif
 
 #if defined(XALAN_NO_COVARIANT_RETURN_TYPE)
 	virtual Function*
