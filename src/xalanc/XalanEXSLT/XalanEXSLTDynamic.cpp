dd36d3d5701a30c25e657a1cc44bc4efbe05d501
David N Bertoni <dbertoni@apache.org>
2003-09-04 07:55:19 +0000

    Initial revision.

---
+++ src/xalanc/XalanEXSLT/XalanEXSLTDynamic.cpp
@@ -0,0 +1,187 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 2002-2003 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Xalan" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 1999, International
+ * Business Machines, Inc., http://www.ibm.com.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+#include "XalanEXSLTDynamic.hpp"
+#include "XalanEXSLTDynamicImpl.hpp"
+
+
+
+#include <xalanc/PlatformSupport/XalanUnicode.hpp>
+
+
+
+#include <xalanc/XPath/XObjectFactory.hpp>
+#include <xalanc/XPath/XPathEnvSupportDefault.hpp>
+
+
+
+XALAN_CPP_NAMESPACE_BEGIN
+
+
+
+XObjectPtr
+XalanEXSLTFunctionEvaluate::execute(
+			XPathExecutionContext&			executionContext,
+			XalanNode*						context,
+			const XObjectArgVectorType&		args,
+			const LocatorType*				locator) const
+{
+	try
+	{
+		return ParentType::execute(executionContext, context, args, locator);
+	}
+	catch(const XSLException&)
+	{
+	}
+
+	XPathExecutionContext::BorrowReturnMutableNodeRefList	theGuard(executionContext);
+
+	return executionContext.getXObjectFactory().createNodeSet(theGuard);
+}
+
+
+
+static const XalanDOMChar	s_dynamicNamespace[] =
+{
+	XalanUnicode::charLetter_h,
+	XalanUnicode::charLetter_t,
+	XalanUnicode::charLetter_t,
+	XalanUnicode::charLetter_p,
+	XalanUnicode::charColon,
+	XalanUnicode::charSolidus,
+	XalanUnicode::charSolidus,
+	XalanUnicode::charLetter_e,
+	XalanUnicode::charLetter_x,
+	XalanUnicode::charLetter_s,
+	XalanUnicode::charLetter_l,
+	XalanUnicode::charLetter_t,
+	XalanUnicode::charFullStop,
+	XalanUnicode::charLetter_o,
+	XalanUnicode::charLetter_r,
+	XalanUnicode::charLetter_g,
+	XalanUnicode::charSolidus,
+	XalanUnicode::charLetter_d,
+	XalanUnicode::charLetter_y,
+	XalanUnicode::charLetter_n,
+	XalanUnicode::charLetter_a,
+	XalanUnicode::charLetter_m,
+	XalanUnicode::charLetter_i,
+	XalanUnicode::charLetter_c,
+	0
+};
+
+
+
+static const XalanDOMChar	s_evaluateFunctionName[] =
+{
+	XalanUnicode::charLetter_e,
+	XalanUnicode::charLetter_v,
+	XalanUnicode::charLetter_a,
+	XalanUnicode::charLetter_l,
+	XalanUnicode::charLetter_u,
+	XalanUnicode::charLetter_a,
+	XalanUnicode::charLetter_t,
+	XalanUnicode::charLetter_e,
+	0
+};
+
+
+
+static const XalanEXSLTFunctionEvaluate		s_evaluateFunction;
+
+
+
+static const XalanEXSLTDynamicFunctionsInstaller::FunctionTableEntry	theFunctionTable[] =
+{
+	{ s_evaluateFunctionName, &s_evaluateFunction},
+	{ 0, 0 }
+};
+
+
+
+void
+XalanEXSLTDynamicFunctionsInstaller::installLocal(XPathEnvSupportDefault&	theSupport)
+{
+	doInstallLocal(s_dynamicNamespace, theFunctionTable, theSupport);
+}
+
+
+
+void
+XalanEXSLTDynamicFunctionsInstaller::installGlobal()
+{
+	doInstallGlobal(s_dynamicNamespace, theFunctionTable);
+}
+
+
+
+void
+XalanEXSLTDynamicFunctionsInstaller::uninstallLocal(XPathEnvSupportDefault&	theSupport)
+{
+	doUninstallLocal(s_dynamicNamespace, theFunctionTable, theSupport);
+}
+
+
+
+void
+XalanEXSLTDynamicFunctionsInstaller::uninstallGlobal()
+{
+	doUninstallGlobal(s_dynamicNamespace, theFunctionTable);
+}
+
+
+
+XALAN_CPP_NAMESPACE_END
