5f72546bb5224b8bd204ee1d505adec783467730
Matthew Hoyt <mhoyt@apache.org>
2004-08-20 20:23:35 +0000

    Fix incorrect cast (causes assertion on VC7.1)

--- src/xalanc/PlatformSupport/ReusableArenaBlock.hpp
+++ src/xalanc/PlatformSupport/ReusableArenaBlock.hpp
@@ -62,7 +62,7 @@ public:
 		bool
 		isValidFor( size_type  rightBorder ) const
 		{
-			return ( ( verificationStamp == size_type(VALID_OBJECT_STAMP)) &&
+			return ( ( verificationStamp == int(VALID_OBJECT_STAMP)) &&
 				( next <= rightBorder ) ) ? true : false ;
 		}
 	};
