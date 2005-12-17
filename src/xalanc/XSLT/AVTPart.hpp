0013677556fb9c80be513484370585c6692fd033
David N Bertoni <dbertoni@apache.org>
2005-12-17 06:24:06 +0000

    Removed Id tag.

--- src/xalanc/XSLT/AVTPart.hpp
+++ src/xalanc/XSLT/AVTPart.hpp
@@ -16,12 +16,7 @@
 #if !defined(XALAN_AVTPART_HEADER_GUARD)
 #define XALAN_AVTPART_HEADER_GUARD 
 
-/**
- * $Id$
- * 
- * $State$
- * 
- */
+
 
 // Base include file.  Must be first.
 #include "XSLTDefinitions.hpp"
