b1031953370d3a82cb9cfc47fefe73f8fa70810c
David N Bertoni <dbertoni@apache.org>
2005-12-17 06:29:33 +0000

    Removed Id tag.

--- src/xalanc/XSLT/ProblemListener.hpp
+++ src/xalanc/XSLT/ProblemListener.hpp
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- * $Id$
- */
 #if !defined(XALAN_PROBLEMLISTENER_HEADER_GUARD)
 #define XALAN_PROBLEMLISTENER_HEADER_GUARD
 
