b3c9cd5402800933c964aaa7ce6bb03d61db88c1
Roger Leigh <rleigh@codelibre.net>
2020-05-20 22:16:04 +0100

    XALANC-800: cmake: Check for time and isnan functions

---
+++ cmake/XalanFunctions.cmake
@@ -0,0 +1,40 @@
+# CMake build for xerces-c
+#
+# Written by Roger Leigh <rleigh@codelibre.net>
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# function checks
+
+include(CheckFunctionExists)
+include(CheckCXXSourceCompiles)
+
+check_function_exists(gmtime_r XALAN_HAVE_GMTIME_R)
+check_function_exists(localtime_r XALAN_HAVE_LOCALTIME_R)
+check_function_exists(isnan XALAN_HAVE_ISNAN)
+check_function_exists(_isnan XALAN_HAVE__ISNAN)
+
+
+check_cxx_source_compiles("
+#include <cmath>
+#include <cfloat>
+
+int main() {
+  std::isnan(0.0);
+  std::isnan(NAN);
+}"
+XALAN_HAVE_STD_ISNAN)
+
